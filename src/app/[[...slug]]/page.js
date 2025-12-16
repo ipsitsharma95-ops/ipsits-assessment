2378ba426234bdee3da4ecafb6c1230b1a004ade
ipsitsharma95-ops <ipsitsharma95@gmail.com>
2025-12-16 16:20:42 +0000

    Live update fix

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -1,27 +1,13 @@
-// app/[...slug]/page.js
-import StoryblokPage from './StoryblokPage';
+import StoryblokLiveStory from './StoryblokLiveStory';
 import { getStoryblokApi } from '@/lib/storyblok';
 
 export default async function Page({ params }) {
-  const slug = params?.slug?.join('/') || 'home';
-
-  const storyblokApi = getStoryblokApi();
-  const { data } = await storyblokApi.get(`cdn/stories/${slug}`, {
-    version: 'draft', // REQUIRED for live editing
-  });
-
-<<<<<<< HEAD
-	let sbParams = {
-		version: 'draft',
-	};
+	const slug = params?.slug?.join('/') || 'home';
 
 	const storyblokApi = getStoryblokApi();
-	let { data } = await storyblokApi.get(`cdn/stories/${fullSlug}`, sbParams);
+	const { data } = await storyblokApi.get(`cdn/stories/${slug}`, {
+		version: 'draft', // 🔴 REQUIRED for live editing
+	});
 
-	return <StoryblokStory story={data.story} />;
+	return <StoryblokLiveStory story={data.story} />;
 }
-=======
-  return <StoryblokPage story={data.story} />;
-}
-
->>>>>>> 71ada8e (Live update)
