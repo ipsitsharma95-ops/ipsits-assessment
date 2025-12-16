90030df144b958611d8434f57ba80ef0c3e627ed
ipsitsharma95-ops <ipsitsharma95@gmail.com>
2025-12-16 16:37:22 +0000

    Revert "revert"
    This reverts commit 72da52b030355645ea4625da6e87b10d2d921d0a.

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -2,7 +2,17 @@ import StoryblokLiveStory from './StoryblokLiveStory';
 import { getStoryblokApi } from '@/lib/storyblok';
 
 export default async function Page({ params }) {
+<<<<<<< HEAD
 	const slug = params?.slug?.join('/') || 'home';
+=======
+	const { slug } = await params;
+
+	let fullSlug = slug ? slug.join('/') : 'home';
+
+	let sbParams = {
+		version: 'published',
+	};
+>>>>>>> parent of 72da52b (revert)
 
 	const storyblokApi = getStoryblokApi();
 	const { data } = await storyblokApi.get(`cdn/stories/${slug}`, {
